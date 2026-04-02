e1b4c58dfbfcd792de1eefc19510c89155d1b4bf
UnorthodoxMERC <kboomer224@aol.com>
2026-04-01 21:58:57 -0500

    first commit of the start of server

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	handler := http.FileServer(http.Dir("."))
+
+	mux.Handle("/", handler)
+
+	log.Fatal(srv.ListenAndServe())
+
 
 }
