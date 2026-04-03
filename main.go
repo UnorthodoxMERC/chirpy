e8fdda1e5d73aa7b0f482fa4dd35f07d1f934e82
UnorthodoxMERC <kboomer224@aol.com>
2026-04-02 20:49:33 -0500

    added a readiness endpoint

--- main.go
+++ main.go
@@ -14,7 +14,13 @@ func main() {
 
 	handler := http.FileServer(http.Dir("."))
 
-	mux.Handle("/", handler)
+	mux.Handle("/app/", http.StripPrefix("/app", handler))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	log.Fatal(srv.ListenAndServe())
 
